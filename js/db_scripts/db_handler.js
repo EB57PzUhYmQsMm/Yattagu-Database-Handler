32dcc9f09c16c4ae3fbea0b9658e4a97e6b7c9f7
Wyatt <24616883+Yattagu@users.noreply.github.com>
2021-08-18 13:38:41 -0400

    Add files via upload

--- js/db_scripts/db_handler.js
+++ js/db_scripts/db_handler.js
@@ -170,7 +170,7 @@ if (typeof module !== 'undefined') {
   module.exports = saveAs;
 }
 
-var template_blob = "|||||||sample_table||||1||column_1||string||3600|||2||column_2||int||120|||3||column_3||bool||0///////1||gamer||69||true|||2||ninja||[nil]||false"
+var template_blob = "|||||||sample_table||||1||id||int||120|||2||silly_string||string||3600|||3||chad_level||int||120|||4||is_cool||bool||0///////1||gamer||69||true|||2||ninja||[nil]||false"
 
 function create_db(dbname) {
   var blob = new Blob([dbname+template_blob],
